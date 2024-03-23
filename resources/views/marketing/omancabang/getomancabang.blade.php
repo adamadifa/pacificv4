363a68f783e2c419d9638344420ea91c4da1b705
adam adifam <adamabdi.al.a@gmail.com>
2024-03-23 18:53:12 +0700

    update

--- resources/views/marketing/omancabang/getomancabang.blade.php
+++ resources/views/marketing/omancabang/getomancabang.blade.php
@@ -6,24 +6,30 @@
             {{ $d->kode_produk }}
         </td>
         <td>{{ $d->nama_produk }}</td>
-        <td>
-            <input type="text" id="jmlm1" name="jmlm1[]" class="jmlm1 text-end form-oman number-separator"
+        <td class="text-center">
+            {{ formatAngka($d->minggu_1) }}
+            <input type="hidden" id="jmlm1" name="jmlm1[]" class="jmlm1 text-end form-oman number-separator"
                 placeholder="0" autocomplete="false" aria-autocomplete="list" value="{{ formatAngka($d->minggu_1) }}">
         </td>
-        <td>
-            <input type="text" id="jmlm2" name="jmlm2[]" class="jmlm2 text-end form-oman number-separator"
+        <td class="text-center">
+            {{ formatAngka($d->minggu_2) }}
+            <input type="hidden" id="jmlm2" name="jmlm2[]" class="jmlm2 text-end form-oman number-separator"
                 placeholder="0" autocomplete="false" aria-autocomplete="list" value="{{ formatAngka($d->minggu_2) }}" />
-        </td>
-        <td>
-            <input type="text" id="jmlm3" name="jmlm3[]" class="jmlm3 text-end form-oman number-separator"
+        </td class="text-center">
+        <td class="text-center">
+            {{ formatAngka($d->minggu_3) }}
+            <input type="hidden" id="jmlm3" name="jmlm3[]" class="jmlm3 text-end form-oman number-separator"
                 placeholder="0" autocomplete="false" aria-autocomplete="list" value="{{ formatAngka($d->minggu_3) }}" />
         </td>
-        <td>
-            <input type="text" id="jmlm4" name="jmlm4[]" class="jmlm4 text-end form-oman number-separator"
-                placeholder="0" autocomplete="false" aria-autocomplete="list" value="{{ formatAngka($d->minggu_4) }}" />
+        <td class="text-center">
+            {{ formatAngka($d->minggu_4) }}
+            <input type="hidden" id="jmlm4" name="jmlm4[]" class="jmlm4 text-end form-oman number-separator"
+                placeholder="0" autocomplete="false" aria-autocomplete="list"
+                value="{{ formatAngka($d->minggu_4) }}" />
         </td>
-        <td>
-            <input type="text" id="subtotal" name="subtotal[]" class="subtotal text-end form-oman" placeholder="0"
+        <td class="text-center">
+            {{ formatAngka($d->total) }}
+            <input type="hidden" id="subtotal" name="subtotal[]" class="subtotal text-end form-oman" placeholder="0"
                 value="{{ formatAngka($d->total) }}" readonly />
         </td>
     </tr>
