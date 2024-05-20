adb5fce1beb0b876bd0403c12191ce98cdbf7c68
adam adifam <adamabdi.al.a@gmail.com>
2024-05-21 06:41:54 +0700

    update

---
+++ app/Models/Detailtransfer.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Detailtransfer extends Model
+{
+    use HasFactory;
+}
