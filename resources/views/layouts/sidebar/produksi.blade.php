2ef5d375286c62b64f8421edae283b56813b279d
adam adifam <adamabdi.al.a@gmail.com>
2024-09-02 21:52:13 +0700

    update

--- resources/views/layouts/sidebar/produksi.blade.php
+++ resources/views/layouts/sidebar/produksi.blade.php
@@ -1,5 +1,5 @@
 @if (auth()->user()->hasAnyPermission($produksi_permission))
-    <li class="menu-item {{ request()->is($produksi_request) ? 'active' : '' }}">
+    <li class="menu-item {{ request()->is($produksi_request) ? 'open' : '' }}">
 
         <a href="javascript:void(0);" class="menu-link menu-toggle">
             <i class="menu-icon tf-icons ti ti-box"></i>
