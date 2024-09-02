f6137f72bbb2cd53e23851ea5d7c024d42ae75ba
adam adifam <adamabdi.al.a@gmail.com>
2024-09-02 21:53:54 +0700

    update

--- resources/views/layouts/sidebar/worksheetom.blade.php
+++ resources/views/layouts/sidebar/worksheetom.blade.php
@@ -15,21 +15,21 @@
         ]))
     <li
         class="menu-item {{ request()->is([
-            '/worksheetom/omancabang',
-            '/worksheetom/oman',
-            '/worksheetom/komisisalesman',
-            '/worksheetom/insentifom',
-            '/worksheetom/komisidriverhelper',
-            '/worksheetom/costratio',
-            '/worksheetom/visitpelanggan',
-            '/worksheetom/monitoringretur',
-            '/worksheetom/monitoringprogram',
-            '/worksheetom/kebutuhancabang',
-            '/worksheetom/produkexpired',
-            '/worksheetom/evaluasisharing',
-            '/worksheetom/bbm',
-            '/worksheetom/ratiobs',
-            '/worksheetom/*',
+            'worksheetom/omancabang',
+            'worksheetom/oman',
+            'worksheetom/komisisalesman',
+            'worksheetom/insentifom',
+            'worksheetom/komisidriverhelper',
+            'worksheetom/costratio',
+            'worksheetom/visitpelanggan',
+            'worksheetom/monitoringretur',
+            'worksheetom/monitoringprogram',
+            'worksheetom/kebutuhancabang',
+            'worksheetom/produkexpired',
+            'worksheetom/evaluasisharing',
+            'worksheetom/bbm',
+            'worksheetom/ratiobs',
+            'worksheetom/*',
         ])
             ? 'open'
             : '' }}">
@@ -39,79 +39,79 @@
             <div>Worksheet OM</div>
         </a>
         <ul class="menu-sub">
-            <li class="menu-item {{ request()->is('/worksheetom/oman') ? 'active' : '' }}">
+            <li class="menu-item {{ request()->is('worksheetom/oman') ? 'active' : '' }}">
                 <a href="{{ route('worksheetom.oman') }}" class="menu-link">
                     <i class="menu-icon tf-icons ti ti-box"></i>
                     <div>OMAN</div>
                 </a>
             </li>
-            <li class="menu-item {{ request()->is('/worksheetom/komisisalesman') ? 'active' : '' }}">
+            <li class="menu-item {{ request()->is('worksheetom/komisisalesman') ? 'active' : '' }}">
                 <a href="{{ route('worksheetom.komisisalesman') }}" class="menu-link">
                     <i class="menu-icon tf-icons ti ti-box"></i>
                     <div>Komisi Salesman</div>
                 </a>
             </li>
-            <li class="menu-item {{ request()->is('/worksheetom/insentifom') ? 'active' : '' }}">
+            <li class="menu-item {{ request()->is('worksheetom/insentifom') ? 'active' : '' }}">
                 <a href="{{ route('worksheetom.insentifom') }}" class="menu-link">
                     <i class="menu-icon tf-icons ti ti-box"></i>
                     <div>Insentif OM</div>
                 </a>
             </li>
-            <li class="menu-item {{ request()->is('/worksheetom/komisidriverhelper') ? 'active' : '' }}">
+            <li class="menu-item {{ request()->is('worksheetom/komisidriverhelper') ? 'active' : '' }}">
                 <a href="{{ route('worksheetom.komisidriverhelper') }}" class="menu-link">
                     <i class="menu-icon tf-icons ti ti-box"></i>
                     <div>Komisi Driver Helper</div>
                 </a>
             </li>
-            <li class="menu-item {{ request()->is('/worksheetom/costratio') ? 'active' : '' }}">
+            <li class="menu-item {{ request()->is('worksheetom/costratio') ? 'active' : '' }}">
                 <a href="{{ route('worksheetom.costratio') }}" class="menu-link">
                     <i class="menu-icon tf-icons ti ti-box"></i>
                     <div>Cost Ratio</div>
                 </a>
             </li>
-            <li class="menu-item {{ request()->is('/worksheetom/visitpelanggan') ? 'active' : '' }}">
+            <li class="menu-item {{ request()->is('worksheetom/visitpelanggan') ? 'active' : '' }}">
                 <a href="{{ route('worksheetom.visitpelanggan') }}" class="menu-link">
                     <i class="menu-icon tf-icons ti ti-box"></i>
                     <div>Visit Pelanggan</div>
                 </a>
             </li>
-            <li class="menu-item {{ request()->is('/worksheetom/monitoringretur') ? 'active' : '' }}">
+            <li class="menu-item {{ request()->is('worksheetom/monitoringretur') ? 'active' : '' }}">
                 <a href="{{ route('worksheetom.monitoringretur') }}" class="menu-link">
                     <i class="menu-icon tf-icons ti ti-box"></i>
                     <div>Monitoring Retur</div>
                 </a>
             </li>
-            <li class="menu-item {{ request()->is('/worksheetom/monitoringprogram') ? 'active' : '' }}">
+            <li class="menu-item {{ request()->is('worksheetom/monitoringprogram') ? 'active' : '' }}">
                 <a href="{{ route('worksheetom.monitoringprogram') }}" class="menu-link">
                     <i class="menu-icon tf-icons ti ti-box"></i>
                     <div>Monitoring Program</div>
                 </a>
             </li>
-            <li class="menu-item {{ request()->is('/worksheetom/kebutuhancabang') ? 'active' : '' }}">
+            <li class="menu-item {{ request()->is('worksheetom/kebutuhancabang') ? 'active' : '' }}">
                 <a href="{{ route('worksheetom.kebutuhancabang') }}" class="menu-link">
                     <i class="menu-icon tf-icons ti ti-box"></i>
                     <div>Kebutuhan Cabang</div>
                 </a>
             </li>
-            <li class="menu-item {{ request()->is('/worksheetom/produkexpired') ? 'active' : '' }}">
+            <li class="menu-item {{ request()->is('worksheetom/produkexpired') ? 'active' : '' }}">
                 <a href="{{ route('worksheetom.produkexpired') }}" class="menu-link">
                     <i class="menu-icon tf-icons ti ti-box"></i>
                     <div>Produk Expired</div>
                 </a>
             </li>
-            <li class="menu-item {{ request()->is('/worksheetom/evaluasisharing') ? 'active' : '' }}">
+            <li class="menu-item {{ request()->is('worksheetom/evaluasisharing') ? 'active' : '' }}">
                 <a href="{{ route('worksheetom.evaluasisharing') }}" class="menu-link">
                     <i class="menu-icon tf-icons ti ti-box"></i>
                     <div>Evaluasi Sharing</div>
                 </a>
             </li>
-            <li class="menu-item {{ request()->is('/worksheetom/bbm') ? 'active' : '' }}">
+            <li class="menu-item {{ request()->is('worksheetom/bbm') ? 'active' : '' }}">
                 <a href="{{ route('worksheetom.bbm') }}" class="menu-link">
                     <i class="menu-icon tf-icons ti ti-box"></i>
                     <div>BBM</div>
                 </a>
             </li>
-            <li class="menu-item {{ request()->is('/worksheetom/ratiobs') ? 'active' : '' }}">
+            <li class="menu-item {{ request()->is('worksheetom/ratiobs') ? 'active' : '' }}">
                 <a href="{{ route('worksheetom.ratiobs') }}" class="menu-link">
                     <i class="menu-icon tf-icons ti ti-box"></i>
                     <div>Ratio BS</div>
