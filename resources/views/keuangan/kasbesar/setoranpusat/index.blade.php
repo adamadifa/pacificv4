0436668f480ded8fe488cd2565e4cad5ea66b8e1
adam adifam <adamabdi.al.a@gmail.com>
2024-09-09 01:49:50 +0700

    update

--- resources/views/keuangan/kasbesar/setoranpusat/index.blade.php
+++ resources/views/keuangan/kasbesar/setoranpusat/index.blade.php
@@ -56,11 +56,11 @@
                                 <table class="table  table-bordered">
                                     <thead class="table-dark">
                                         <tr>
-                                            <th rowspan="2" class="align-middle" style="width: 5%">Tanggal</th>
+                                            <th rowspan="2" class="align-middle">Tanggal</th>
                                             <th rowspan="2" style="width:20%">Keterangan</th>
                                             <th colspan="4" class="text-center">Setoran</th>
                                             <th rowspan="2" class="align-middle">Jumlah</th>
-                                            <th rowspan="2" class="text-center align-middle" style="width: 10%">BANK</th>
+                                            <th rowspan="2" class="text-center align-middle" style="width: 15%">BANK</th>
                                             <th rowspan="2" class="text-center align-middle">Status</th>
                                             <th rowspan="2" class="text-center align-middle">#</th>
                                         </tr>
