f156100005ac330b0908c83f248316fc3e538dcc
adam adifam <adamabdi.al.a@gmail.com>
2024-09-09 07:12:59 +0700

    update

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -44,7 +44,7 @@ class DashboardController extends Controller
             return $this->salesman();
         } else if ($user->hasRole('admin penjualan')) {
             return $this->operationmanager();
-        } else if ($user->hasRole('gm operasional')) {
+        } else if ($user->hasRole('gm operasional') || $user->hasRole('spv produksi')) {
             return $this->operasional();
         } else {
             return $this->dashboarddefault();
