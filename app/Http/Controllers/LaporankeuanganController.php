650792e618ed0f9e9f34e0cf2853a46c4533664d
Adam Adifa <adamabdi.al.a@gmail.com>
2024-09-09 13:39:09 +0700

    update

--- app/Http/Controllers/LaporankeuanganController.php
+++ app/Http/Controllers/LaporankeuanganController.php
@@ -1776,6 +1776,15 @@ class LaporankeuanganController extends Controller
         $qsaldoawal->selectRaw("SUM(IF( `debet_kredit` = 'K', jumlah, 0)) -SUM(IF( `debet_kredit` = 'D', jumlah, 0)) as saldo_awal");
         $qsaldoawal->whereBetween('tanggal', [$awal_kas_kecil, $sehariSebelumDari]);
         $qsaldoawal->where('kode_cabang', $request->kode_cabang);
+        if (!$user->hasRole($roles_access_all_cabang)) {
+            if ($user->hasRole('regional sales manager')) {
+                $qsaldoawal->where('kode_cabang', $request->kode_cabang);
+            } else {
+                $qsaldoawal->where('kode_cabang', auth()->user()->kode_cabang);
+            }
+        } else {
+            $qsaldoawal->where('kode_cabang', $request->kode_cabang);
+        }
         $saldoawal = $qsaldoawal->first();
 
 
