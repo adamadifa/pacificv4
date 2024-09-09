dfcd69ffa745c04e7eadaf6421a24d5173fcef31
adam adifam <adamabdi.al.a@gmail.com>
2024-09-09 08:46:50 +0700

    update

--- app/Http/Controllers/PembayarantransferController.php
+++ app/Http/Controllers/PembayarantransferController.php
@@ -15,6 +15,7 @@ use App\Models\Salesman;
 use App\Models\Setoranpusat;
 use App\Models\Setoranpusattransfer;
 use App\Models\Transfer;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Crypt;
 use Illuminate\Support\Facades\DB;
@@ -26,6 +27,9 @@ class PembayarantransferController extends Controller
     public function index(Request $request)
     {
 
+        $user = User::findorfail(auth()->user()->id);
+        $roles_access_all_cabang = config('global.roles_access_all_cabang');
+        dd($roles_access_all_cabang);
         if (!empty($request->dari) && !empty($request->sampai)) {
             if (lockreport($request->dari) == "error") {
                 return Redirect::back()->with(messageError('Data Tidak Ditemukan'));
