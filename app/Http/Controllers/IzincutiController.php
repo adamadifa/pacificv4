d377d291463f95ef35e83dae391a7255b35f0e9a
adam adifam <adamabdi.al.a@gmail.com>
2024-09-23 10:09:20 +0700

    update

--- app/Http/Controllers/IzincutiController.php
+++ app/Http/Controllers/IzincutiController.php
@@ -238,7 +238,8 @@ class IzincutiController extends Controller
         $user = User::find(auth()->user()->id);
         $i_cuti = new Izincuti();
         $izincuti = $i_cuti->getIzincuti(kode_izin_cuti: $kode_izin_cuti)->first();
-        dd($kode_izin_cuti . " " . DB::table('hrd_izincuti')->where('kode_izin_cuti', $kode_izin_cuti)->first());
+        $datacuti = DB::table('hrd_izincuti')->where('kode_izin_cuti', $kode_izin_cuti)->first();
+        dd($kode_izin_cuti . " " . $datacuti);
         $data['izincuti'] = $izincuti;
 
         $role = $user->getRoleNames()->first();
