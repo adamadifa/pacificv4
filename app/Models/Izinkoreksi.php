94ea6aa5013030caf019c352a14e65f9f5637ebf
adam adifam <adamabdi.al.a@gmail.com>
2024-09-23 19:38:37 +0700

    update

--- app/Models/Izinkoreksi.php
+++ app/Models/Izinkoreksi.php
@@ -67,7 +67,7 @@ class Izinkoreksi extends Model
                 $query->where('cabang.kode_regional', auth()->user()->kode_regional);
             } else if ($user->hasRole('regional operation manager')) { //REG. OPERATION MANAGER
                 $query->where('hrd_izinkoreksi.kode_dept', 'AKT');
-                $query->whereIn('hrd_izinkoreksi.kode_jabatan', ['J05', 'J06']);
+                $query->whereIn('hrd_izinkoreksi.kode_jabatan', ['J08']);
             } else if ($user->hasRole('manager keuangan')) { //MANAGER KEUANGAN
                 $query->where('hrd_izinkoreksi.kode_dept', ['AKT', 'KEU']);
                 $query->where('hrd_izinkoreksi.kode_cabang', 'PST');
@@ -130,7 +130,7 @@ class Izinkoreksi extends Model
                 $query->where('cabang.kode_regional', auth()->user()->kode_regional);
             } else if ($user->hasRole('regional operation manager')) { //REG. OPERATION MANAGER
                 $query->orwhere('hrd_izinkoreksi.kode_dept', 'AKT');
-                $query->whereIn('hrd_izinkoreksi.kode_jabatan', ['J05', 'J06']);
+                $query->whereIn('hrd_izinkoreksi.kode_jabatan', ['J08']);
             } else if ($user->hasRole('manager keuangan')) { //MANAGER KEUANGAN
                 $query->orwhere('hrd_izinkoreksi.kode_dept', ['AKT', 'KEU']);
                 $query->where('hrd_izinkoreksi.kode_cabang', 'PST');
