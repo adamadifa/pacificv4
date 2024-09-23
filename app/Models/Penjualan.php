3573ac58c87dad5d524e2a9e6dc6ab843dc7688c
Adam Adifa <adamabdi.al.a@gmail.com>
2024-09-23 14:16:47 +0700

    update

--- app/Models/Penjualan.php
+++ app/Models/Penjualan.php
@@ -361,7 +361,7 @@ class Penjualan extends Model
         );
         $query->addSelect(DB::raw("(SELECT SUM(subtotal) FROM marketing_penjualan_detail
         INNER JOIN marketing_penjualan ON marketing_penjualan_detail.no_faktur = marketing_penjualan.no_faktur
-        WHERE kode_pelanggan = pelanggan.kode_pelanggan  AND status_batal = 0 AND
+        WHERE kode_pelanggan = pelanggan.kode_pelanggan  AND
         marketing_penjualan.tanggal BETWEEN '$start_date' AND '$end_date') as total_bruto"));
 
         $query->addSelect(DB::raw("(SELECT SUM(subtotal) FROM marketing_retur_detail
