6977a2fa2235bd575baa2dc024703019965c7472
Adam Adifa <adamabdi.al.a@gmail.com>
2024-09-24 14:19:51 +0700

    update

--- app/Models/Izinkoreksi.php
+++ app/Models/Izinkoreksi.php
@@ -53,7 +53,8 @@ class Izinkoreksi extends Model
         $query->leftJoin('roles', 'model_has_roles.role_id', '=', 'roles.id');
         if (!in_array($role, ['super admin', 'asst. manager hrd', 'spv presensi', 'direktur'])) {
             if ($user->hasRole('gm operasional')) {
-                $query->whereIn('hrd_izinkoreksi.kode_dept', ['PDQ']);
+                $query->whereIn('hrd_izinkoreksi.kode_dept', ['PMB', 'GDG', 'MTC', 'PRD', 'GAF', 'HRD']);
+                $query->whereIn('hrd_izinkoreksi.kode_jabatan', ['J05', 'J06']);
             } else if ($user->hasRole('gm administrasi')) { //GM ADMINISTRASI
                 $query->whereIn('hrd_izinkoreksi.kode_dept', ['AKT', 'KEU']);
                 $query->whereIn('hrd_izinkoreksi.kode_jabatan', ['J04', 'J05', 'J06', 'J24', 'J25', 'J26']);
@@ -115,8 +116,7 @@ class Izinkoreksi extends Model
                 $query->where('hrd_izinkoreksi.kode_izin_koreksi', $kode_izin_koreksi);
             }
             if ($user->hasRole('gm operasional')) {
-                $query->orwhereIn('hrd_izinkoreksi.kode_dept', ['PMB', 'GDG', 'MTC', 'PRD', 'GAF', 'HRD']);
-                $query->whereIn('hrd_izinkoreksi.kode_jabatan', ['J05', 'J06']);
+                $query->orWhere('hrd_izinkoreksi.kode_dept', 'PDQ');
             } else if ($user->hasRole('gm administrasi')) { //GM ADMINISTRASI
                 $query->orwhereIn('hrd_izinkoreksi.kode_dept', ['AKT', 'KEU']);
                 $query->whereIn('hrd_izinkoreksi.kode_jabatan', ['J04', 'J05', 'J06', 'J24', 'J25', 'J26']);
