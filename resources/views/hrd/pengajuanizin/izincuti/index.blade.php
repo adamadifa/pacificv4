fe0f3de96d1252eaff5c7075da3207b7e172157c
Adam Adifa <adamabdi.al.a@gmail.com>
2024-09-24 13:49:22 +0700

    update

--- resources/views/hrd/pengajuanizin/izincuti/index.blade.php
+++ resources/views/hrd/pengajuanizin/izincuti/index.blade.php
@@ -207,8 +207,8 @@
                                                     <div class="d-flex">
                                                         @can('izincuti.edit')
                                                             @if (
-                                                                ($d->status === '0' && $d->id_pengirim === $level_user) ||
-                                                                    ($d->status === '0' && $d->id_pengirim === $level_user && $d->posisi_ajuan === $next_role && $level_user != $end_role) ||
+                                                                ($d->status === '0' && $d->id_pengirim == auth()->user()->id) ||
+                                                                    ($d->status === '0' && $d->id_pengirim === auth()->user()->id && $d->posisi_ajuan === $next_role && $level_user != $end_role) ||
                                                                     (in_array($level_user, ['super admin', 'asst. manager hrd']) && $d->status === '0'))
                                                                 <a href="#" class="btnEdit me-1"
                                                                     kode_izin_cuti = "{{ Crypt::encrypt($d->kode_izin_cuti) }}">
@@ -272,8 +272,8 @@
                                                         @endcan
                                                         @can('izincuti.delete')
                                                             @if (
-                                                                ($d->status === '0' && $d->id_pengirim === $level_user) ||
-                                                                    ($d->status === '0' && $d->id_pengirim === $level_user && $d->posisi_ajuan === $next_role && $level_user != $end_role) ||
+                                                                ($d->status === '0' && $d->id_pengirim === auth()->user()->id) ||
+                                                                    ($d->status === '0' && $d->id_pengirim === auth()->user()->id && $d->posisi_ajuan === $next_role && $level_user != $end_role) ||
                                                                     (in_array($level_user, ['super admin', 'asst. manager hrd']) && $d->status === '0'))
                                                                 <form class="delete-form me-1"
                                                                     action="{{ route('izincuti.delete', Crypt::encrypt($d->kode_izin_cuti)) }}"
