f8c70d7c619146c805e81052bacf4fd78f1908fc
adam adifam <adamabdi.al.a@gmail.com>
2024-09-27 11:22:29 +0700

    update'

--- resources/views/presensi/getdatamesin.blade.php
+++ resources/views/presensi/getdatamesin.blade.php
@@ -68,12 +68,23 @@
                     <td>{{ $d->status_scan % 2 == 0 ? 'IN' : 'OUT' }} ({{ $d->status_scan }})</td>
                     <td>{{ date('d-m-Y H:i:s', strtotime($d->scan_date)) }}</td>
                     <td>
-                        <div class="btn-group">
-                            <a
-                                href="/presensi/{{ Crypt::encrypt($d->pin) }}/0/{{ date('Y-m-d H:i:s', strtotime($d->scan_date)) }}/updatefrommachine">
-                                <i class="feather icon-log-in success mr-1"></i> Masuk </a>
-                            <a href="/presensi/{{ Crypt::encrypt($d->pin) }}/1/{{ date('Y-m-d H:i:s', strtotime($d->scan_date)) }}/updatefrommachine"
-                                class="ml-1"> <i class="feather icon-log-out danger mr-1"></i> Pulang </a>
+                        <div class="d-flex">
+                            <form method="POST" name="updatemasuk" class="updatemasuk me-1"
+                                action="{{ route('presensi.updatefrommachine', [Crypt::encrypt($d->pin), 0]) }}">
+                                @csrf
+                                <input type="hidden" name="scan_date" value="{{ date('Y-m-d H:i:s', strtotime($d->scan_date)) }}">
+                                <button href="#" class="btn btn-success btn-sm me-1">
+                                    <i class="ti ti-login me-1"></i> Masuk
+                                </button>
+                            </form>
+                            <form method="POST" name="updatepulang" class="updatepulang"
+                                action="{{ route('presensi.updatefrommachine', [Crypt::encrypt($d->pin), 1]) }}">
+                                @csrf
+                                <input type="hidden" name="scan_date" value="{{ date('Y-m-d H:i:s', strtotime($d->scan_date)) }}">
+                                <button href="#" class="btn btn-danger btn-sm me-1">
+                                    <i class="ti ti-logout me-1"></i> Pulang
+                                </button>
+                            </form>
                         </div>
                     </td>
                 </tr>
