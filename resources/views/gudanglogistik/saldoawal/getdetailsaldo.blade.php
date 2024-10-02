db9906ca012cf3a37367dfdca2ef4f16120827de
Adam Adifa <adamabdi.al.a@gmail.com>
2024-10-02 10:30:18 +0700

    update

--- resources/views/gudanglogistik/saldoawal/getdetailsaldo.blade.php
+++ resources/views/gudanglogistik/saldoawal/getdetailsaldo.blade.php
@@ -1,49 +1,49 @@
 @foreach ($barang as $d)
-   @php
-      $saldo_akhir = $d->saldo_awal_jumlah + $d->bm_jumlah - $d->bk_jumlah;
-      $jumlah_saldoawal_pemasukan = $d->qtysaldoawal + $d->qtypemasukan;
-      if (empty($jumlah_saldoawal_pemasukan)) {
-          $jumlah_saldoawal_pemasukan = 1;
-      }
+    @php
+        $saldo_akhir = $d->saldo_awal_jumlah + $d->bm_jumlah - $d->bk_jumlah;
+        $jumlah_saldoawal_pemasukan = $d->qtysaldoawal + $d->qtypemasukan;
+        if (empty($jumlah_saldoawal_pemasukan)) {
+            $jumlah_saldoawal_pemasukan = 1;
+        }
 
-      if (empty($d->saldo_awal_harga) and $d->saldo_awal_harga == 0) {
-          $saldo_akhir_harga = $d->bm_totalharga;
-      } elseif (empty($d->bm_harga) and $d->bm_harga == 0) {
-          $saldo_akhir_harga = $d->saldo_awal_harga;
-      } else {
-          $saldo_akhir_harga = ($d->saldo_awal_totalharga * 1 + $d->bm_totalharga * 1) / $jumlah_saldoawal_pemasukan;
-      }
-   @endphp
-   @if (!empty($saldo_akhir))
-      <tr>
-         <td>
-            <input type="hidden" name="kode_barang[]" value="{{ $d->kode_barang }}">
-            {{ $d->kode_barang }}
-         </td>
-         <td>{{ $d->nama_barang }}</td>
-         <td class="text-end">
-            @if ($readonly)
-               <input type="hidden" name="jumlah[]" value="{{ formatAngkaDesimal($saldo_akhir) }}">
-               {{ formatAngkaDesimal($saldo_akhir) }}
-            @else
-               <input type="text" name="jumlah[]" value="" class="noborder-form text-end number-separator">
-            @endif
-         </td>
-         <td class="text-end">
-            @if ($readonly)
-               <input type="hidden" name="harga[]" value="{{ formatAngkaDesimal($saldo_akhir_harga) }}">
-               {{ formatAngkaDesimal($saldo_akhir_harga) }}
-            @else
-               <input type="text" name="harga[]" value="" class="noborder-form text-end number-separator">
-            @endif
-         </td>
-      </tr>
-   @endif
+        if (empty($d->saldo_awal_harga) and $d->saldo_awal_harga == 0) {
+            $saldo_akhir_harga = $d->bm_totalharga;
+        } elseif (empty($d->bm_harga) and $d->bm_harga == 0) {
+            $saldo_akhir_harga = $d->saldo_awal_harga;
+        } else {
+            $saldo_akhir_harga = 0;
+        }
+    @endphp
+    @if (!empty($saldo_akhir))
+        <tr>
+            <td>
+                <input type="hidden" name="kode_barang[]" value="{{ $d->kode_barang }}">
+                {{ $d->kode_barang }}
+            </td>
+            <td>{{ $d->nama_barang }}</td>
+            <td class="text-end">
+                @if ($readonly)
+                    <input type="hidden" name="jumlah[]" value="{{ formatAngkaDesimal($saldo_akhir) }}">
+                    {{ formatAngkaDesimal($saldo_akhir) }}
+                @else
+                    <input type="text" name="jumlah[]" value="" class="noborder-form text-end number-separator">
+                @endif
+            </td>
+            <td class="text-end">
+                @if ($readonly)
+                    <input type="hidden" name="harga[]" value="{{ formatAngkaDesimal($saldo_akhir_harga) }}">
+                    {{ formatAngkaDesimal($saldo_akhir_harga) }}
+                @else
+                    <input type="text" name="harga[]" value="" class="noborder-form text-end number-separator">
+                @endif
+            </td>
+        </tr>
+    @endif
 @endforeach
 <script>
-   easyNumberSeparator({
-      selector: '.number-separator',
-      separator: '.',
-      decimalSeparator: ',',
-   });
+    easyNumberSeparator({
+        selector: '.number-separator',
+        separator: '.',
+        decimalSeparator: ',',
+    });
 </script>
