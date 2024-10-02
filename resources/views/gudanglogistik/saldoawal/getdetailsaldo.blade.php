729268faa92c3ab34b858eef828230d68f8ef631
Adam Adifa <adamabdi.al.a@gmail.com>
2024-10-02 12:48:49 +0700

    update

--- resources/views/gudanglogistik/saldoawal/getdetailsaldo.blade.php
+++ resources/views/gudanglogistik/saldoawal/getdetailsaldo.blade.php
@@ -7,7 +7,7 @@
         }
 
         if (empty($d->saldo_awal_harga) and $d->saldo_awal_harga == 0) {
-            $saldo_akhir_harga = !empty($d->bm_jumlah) ? 'OK' : 'NO';
+            $saldo_akhir_harga = !empty($d->bm_jumlah) ? 1 : 99;
         } elseif (empty($d->bm_harga) and $d->bm_harga == 0) {
             $saldo_akhir_harga = $d->saldo_awal_harga;
         } else {
