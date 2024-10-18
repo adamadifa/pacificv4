5bd67e4dc4481b97c8be3261c758609bf21419b6
Adam Adifa <adamabdi.al.a@gmail.com>
2024-10-18 15:38:43 +0700

    update

--- resources/views/hrd/laporan/presensi_cetak.blade.php
+++ resources/views/hrd/laporan/presensi_cetak.blade.php
@@ -144,8 +144,8 @@
                                     }
                                     $total_overtime_libur += $overtime_libur;
 
-                                    $total_premi_shift2_lembur += $lembur['jmlharilembur_shift_2'];
-                                    $total_premi_shift3_lembur += $lembur['jmlharilembur_shift_3'];
+                                    $total_premi_shift2_lembur += $lembur['jmlharilembur_shift_2'] + $lembur_libur['jmlharilembur_shift_2'];
+                                    $total_premi_shift3_lembur += $lembur['jmlharilembur_shift_3'] + $lembur_libur['jmlharilembur_shift_3'];
                                 @endphp
                                 @if (isset($d[$tanggal_presensi]))
                                     @php
