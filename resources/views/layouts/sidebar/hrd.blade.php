7675cf14975ddbdf90902f7310497b89faf692eb
Adam Adifa <adamabdi.al.a@gmail.com>
2024-10-27 23:50:22 +0700

    update

--- resources/views/layouts/sidebar/hrd.blade.php
+++ resources/views/layouts/sidebar/hrd.blade.php
@@ -144,7 +144,7 @@
             @endif
 
             @if (auth()->user()->hasAnyPermission(['hrd.presensi', 'hrd.psm', 'hrd.gaji', 'hrd.slipgaji']))
-                <li class="menu-item {{ request()->is(['izinabsen', 'izinabsen/*']) ? 'active' : '' }}">
+                <li class="menu-item {{ request()->is(['laporanhrd', 'laporanhrd/*']) ? 'active' : '' }}">
                     <a href="{{ route('laporanhrd.index') }}" class="menu-link">
                         <div>Laporan</div>
                     </a>
