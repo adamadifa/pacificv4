9c75c0b57b3545ad8ae2f19a0d0aba8831a4e1f7
Adam Adifa <adamabdi.al.a@gmail.com>
2024-11-14 11:32:13 +0700

    update

--- resources/views/hrd/laporan/rekap_gaji_cetak.blade.php
+++ resources/views/hrd/laporan/rekap_gaji_cetak.blade.php
@@ -788,8 +788,13 @@
                                     $potongan_jam_dirumahkan = 0;
                                     if (!empty($cekdirumahkan)) {
                                         if (getNamahari($tanggal_presensi) == 'Sabtu') {
-                                            $potongan_jam_tidakhadir = 2.5;
-                                            $total_jam = 2.5;
+                                            if ($tanggal_presensi == '2024-10-26') {
+                                                $total_jam = 3.5;
+                                                $potongan_jam_tidakhadir = 3.5;
+                                            } else {
+                                                $total_jam = 2.5;
+                                                $potongan_jam_tidakhadir = 2.5;
+                                            }
                                         } else {
                                             $potongan_jam_tidakhadir = 3.5;
                                             $total_jam = 3.5;
