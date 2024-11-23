609506496d3969193ee44cf008f30b304cb65ce1
adam adifam <adamabdi.al.a@gmail.com>
2024-11-24 05:52:41 +0700

    update

--- app/Http/Controllers/PencairanprogramController.php
+++ app/Http/Controllers/PencairanprogramController.php
@@ -4,11 +4,13 @@ namespace App\Http\Controllers;
 
 use App\Models\Cabang;
 use App\Models\Detailpenjualan;
+use App\Models\Diskon;
 use App\Models\Pencairanprogram;
 use App\Models\Penjualan;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Crypt;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Redirect;
 
 class PencairanprogramController extends Controller
@@ -96,16 +98,26 @@ class PencairanprogramController extends Controller
     {
         if ($request->kode_program == 'PR001') {
             $produk = ['BB', 'DEP'];
+            $kategori_diskon = 'D001';
         } else {
             $produk = ['AB', 'AR', 'AS'];
+            $kategori_diskon = 'D002';
         }
 
         $start_date = $request->tahun . '-' . $request->bulan . '-01';
         $end_date = date('Y-m-t', strtotime($start_date));
 
-        $detailpenjualan = Detailpenjualan::select('marketing_penjualan.kode_pelanggan', 'nama_pelanggan')
+        $detailpenjualan = Detailpenjualan::select(
+            'marketing_penjualan.kode_pelanggan',
+            'nama_pelanggan',
+            DB::raw('floor(jumlah/isi_pcs_dus) as jml_dus'),
+            DB::raw('(SELECT diskon FROM produk_diskon WHERE floor(marketing_penjualan_detail.jumlah/produk.isi_pcs_dus) BETWEEN produk_diskon.min_qty AND produk_diskon.max_qty AND kode_kategori_diskon="D001") as diskon'),
+            DB::raw('floor(jumlah/isi_pcs_dus) * (SELECT diskon FROM produk_diskon WHERE floor(marketing_penjualan_detail.jumlah/produk.isi_pcs_dus) BETWEEN produk_diskon.min_qty AND produk_diskon.max_qty AND kode_kategori_diskon="' . $kategori_diskon . '") as diskon_reguler'),
+
+        )
             ->join('produk_harga', 'marketing_penjualan_detail.kode_harga', '=', 'produk_harga.kode_harga')
-            ->join('marketing_penjualan', 'marketing_penjualan_detail.kode_penjualan', '=', 'marketing_penjualan.kode_penjualan')
+            ->join('produk', 'produk_harga.kode_produk', '=', 'produk.kode_produk')
+            ->join('marketing_penjualan', 'marketing_penjualan_detail.no_faktur', '=', 'marketing_penjualan.no_faktur')
             ->join('salesman', 'marketing_penjualan.kode_salesman', '=', 'salesman.kode_salesman')
             ->join('pelanggan', 'marketing_penjualan.kode_pelanggan', '=', 'pelanggan.kode_pelanggan')
             ->whereBetween('marketing_penjualan.tanggal', [$start_date, $end_date])
@@ -113,10 +125,27 @@ class PencairanprogramController extends Controller
             ->where('status', 1)
             ->whereRaw("datediff(marketing_penjualan.tanggal_pelunasan, marketing_penjualan.tanggal) <= 14")
             ->where('status_batal', 0)
-            ->groupBy('marketing_penjualan.kode_pelanggan', 'nama_pelanggan')
+            ->whereIn('produk_harga.kode_produk', $produk)
+            ->orderBy('nama_pelanggan')
             ->get();
 
-        $data['detail'] = $detailpenjualan;
+        $detail = $detailpenjualan->groupBy('kode_pelanggan')
+            ->map(function ($item) {
+                return [
+                    'kode_pelanggan' => $item->first()->kode_pelanggan,
+                    'nama_pelanggan' => $item->first()->nama_pelanggan,
+                    'jml_dus' => $item->sum('jml_dus'),
+                    'diskon_reguler' => $item->sum('diskon_reguler'),
+                ];
+            })
+            ->sortBy('nama_pelanggan')
+            ->values()
+            ->all();
+
+
+
+        $data['detail'] = $detail;
+        $data['diskon'] = Diskon::where('kode_kategori_diskon', $kategori_diskon)->get();
         return view('worksheetom.pencairanprogram.tambahpelanggan', $data);
     }
 }
--- resources/views/worksheetom/pencairanprogram/tambahpelanggan.blade.php
+++ resources/views/worksheetom/pencairanprogram/tambahpelanggan.blade.php
@@ -1,15 +1,35 @@
 <table class="table">
     <thead class="table-dark">
         <tr>
-            <th>Kode Pelanggan</th>
-            <th>Nama Pelanggan</th>
+            <th rowspan="2">Kode Pelanggan</th>
+            <th rowspan="2">Nama Pelanggan</th>
+            <th rowspan="2">Qty</th>
+            <th colspan="2">Diskon</th>
+        </tr>
+        <tr>
+            <th>Reguler</th>
+            <th>Kumulatif</th>
         </tr>
     </thead>
     <tbody>
         @foreach ($detail as $d)
             <tr>
-                <td>{{ $d->kode_pelanggan }}</td>
-                <td>{{ $d->nama_pelanggan }}</td>
+                <td>{{ $d['kode_pelanggan'] }}</td>
+                <td>{{ $d['nama_pelanggan'] }}</td>
+                <td>{{ $d['jml_dus'] }}</td>
+                <td class="text-end">{{ formatAngka($d['diskon_reguler']) }}</td>
+                <td>
+                    <?php
+                    $diskon_value = 0;
+                    foreach ($diskon as $item) {
+                        if ($d['jml_dus'] >= $item->min_qty && $d['jml_dus'] <= $item->max_qty) {
+                            $diskon_value = $item->diskon;
+                            break;
+                        }
+                    }
+                    ?>
+                    {{ formatAngka($diskon_value) }}
+                </td>
             </tr>
         @endforeach
     </tbody>
