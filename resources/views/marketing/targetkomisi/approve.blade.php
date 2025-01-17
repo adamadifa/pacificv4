4f6a0c4133a3b06b25826e56270f95fcd92bf275
Adam Adifa <adamabdi.al.a@gmail.com>
2025-01-17 13:20:17 +0700

    update

--- resources/views/marketing/targetkomisi/approve.blade.php
+++ resources/views/marketing/targetkomisi/approve.blade.php
@@ -86,10 +86,14 @@
                                 @foreach ($produk as $p)
                                     @php
                                         $rata_rata_penjualan = $d->{"penjualan_$p->kode_produk"} / $p->isi_pcs_dus / 3;
-                                        $jml_penjualan = $d->{"penjualan_$p->kode_produk"} / $p->isi_pcs_dus;
+                                        $jml_penjualan_tigabulan = $d->{"penjualan_tigabulan_$p->kode_produk"} / $p->isi_pcs_dus;
+                                        $jml_penjualan_duabulan = $d->{"penjualan_duabulan_$p->kode_produk"} / $p->isi_pcs_dus;
+                                        $jml_penjualan_lastbulan = $d->{"penjualan_lastbulan_$p->kode_produk"} / $p->isi_pcs_dus;
                                     @endphp
                                     <td class="text-end bg-success text-white"> {{ formatAngka($rata_rata_penjualan) }}</td>
-                                    <td class="text-end bg-info text-white">{{ formatAngka($jml_penjualan) }}</td>
+                                    <td class="text-end bg-info text-white">{{ formatAngka($jml_penjualan_tigabulan) }}</td>
+                                    <td class="text-end bg-warning text-white">{{ formatAngka($jml_penjualan_duabulan) }}</td>
+                                    <td class="text-end bg-danger text-white">{{ formatAngka($jml_penjualan_lastbulan) }}</td>
                                     <td class="text-end">{{ formatAngka($d->{"target_$p->kode_produk"}) }}</td>
                                 @endforeach
                             </tr>
