5a7aa63715445bde515e7fc163075faa31773a7d
Adam Adifa <adamabdi.al.a@gmail.com>
2025-01-17 11:40:37 +0700

    update

--- resources/views/marketing/targetkomisi/show.blade.php
+++ resources/views/marketing/targetkomisi/show.blade.php
@@ -23,49 +23,53 @@
 </div>
 <div class="row mt-2">
     <div class="col">
-        <table class="table table-bordered  table-hover">
-            <thead class="table-dark">
-                <tr>
-                    <th rowspan="2" align="middle">Kode</th>
-                    <th rowspan="2" align="middle">NIK</th>
-                    <th rowspan="2" align="middle">Salesman</th>
-                    <th rowspan="2" align="middle">Masa Kerja</th>
-                    <th colspan="{{ count($produk) * 2 }}" class="text-center">Produk</th>
-                </tr>
-                <tr>
-                    @foreach ($produk as $d)
-                        <th class="text-center" colspan="2">
-                            {{ $d->kode_produk }}
-                        </th>
-                    @endforeach
-                </tr>
-            </thead>
-            <tbody>
-                @foreach ($detail as $d)
+        <div class="table-modal">
+            <table class="table table-bordered  table-hover" style="width: 140%">
+                <thead class="table-dark">
                     <tr>
-                        <td>{{ $d->kode_salesman }}</td>
-                        <td>{{ $d->nik }}</td>
-                        <td>{{ $d->nama_salesman }}</td>
-                        <td>
-                            @php
-                                $end_date = $targetkomisi->tahun . '-' . $targetkomisi->bulan . '-01';
-                                $masakerja = hitungMasakerja($d->tanggal_masuk, $end_date);
-                            @endphp
-                            @if (!empty($d->tanggal_masuk))
-                                {{ $masakerja['tahun'] }} Tahun {{ $masakerja['bulan'] }} Bulan
-                            @endif
-                        </td>
-                        @foreach ($produk as $p)
-                            @php
-                                $jml_penjualan = $d->{"penjualan_$p->kode_produk"} / $p->isi_pcs_dus / 3;
-                            @endphp
-                            <td class="text-end bg-success text-white">{{ formatAngka($jml_penjualan) }}</td>
-                            <td class="text-end">{{ formatAngka($d->{"target_$p->kode_produk"}) }}</td>
+                        <th rowspan="2" align="middle" style="width: 5%">Kode</th>
+                        <th rowspan="2" align="middle" style="width: 5%">NIK</th>
+                        <th rowspan="2" align="middle" style="width: 30%">Salesman</th>
+                        <th rowspan="2" align="middle" style="width: 30%">Masa Kerja</th>
+                        <th colspan="{{ count($produk) * 3 }}" class="text-center">Produk</th>
+                    </tr>
+                    <tr>
+                        @foreach ($produk as $d)
+                            <th class="text-center" colspan="3">
+                                {{ $d->kode_produk }}
+                            </th>
                         @endforeach
                     </tr>
-                @endforeach
-            </tbody>
-        </table>
+                </thead>
+                <tbody>
+                    @foreach ($detail as $d)
+                        <tr>
+                            <td>{{ $d->kode_salesman }}</td>
+                            <td>{{ $d->nik }}</td>
+                            <td>{{ $d->nama_salesman }}</td>
+                            <td>
+                                @php
+                                    $end_date = $targetkomisi->tahun . '-' . $targetkomisi->bulan . '-01';
+                                    $masakerja = hitungMasakerja($d->tanggal_masuk, $end_date);
+                                @endphp
+                                @if (!empty($d->tanggal_masuk))
+                                    {{ $masakerja['tahun'] }} Tahun {{ $masakerja['bulan'] }} Bulan
+                                @endif
+                            </td>
+                            @foreach ($produk as $p)
+                                @php
+                                    $rata_rata_penjualan = $d->{"penjualan_$p->kode_produk"} / $p->isi_pcs_dus / 3;
+                                    $jml_penjualan = $d->{"penjualan_$p->kode_produk"} / $p->isi_pcs_dus;
+                                @endphp
+                                <td class="text-end bg-success text-white"> {{ formatAngka($rata_rata_penjualan) }}</td>
+                                <td class="text-end bg-info text-white">{{ formatAngka($jml_penjualan) }}</td>
+                                <td class="text-end">{{ formatAngka($d->{"target_$p->kode_produk"}) }}</td>
+                            @endforeach
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
     </div>
 </div>
 <div class="row">
@@ -74,6 +78,10 @@
             <tr>
                 <td class="bg-success"></td>
                 <td>Rata Rata Penjualan 3 Bulan Terakhir</td>
+            </tr>
+            <tr>
+                <td class="bg-info"></td>
+                <td>Realisasi Selama 3 Bulan</td>
             </tr>
         </table>
     </div>
