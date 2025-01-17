e8290de296fce10288c5d30eb4783b9dbedd299e
Adam Adifa <adamabdi.al.a@gmail.com>
2025-01-17 14:43:10 +0700

    update

--- resources/views/marketing/targetkomisi/approve.blade.php
+++ resources/views/marketing/targetkomisi/approve.blade.php
@@ -35,7 +35,7 @@
     <div class="row mt-2">
         <div class="col">
             <div class="table-modal">
-                <table class="table table-bordered  table-hover" style="width: 240%">
+                <table class="table table-bordered  table-hover" style="width: 270%">
                     <thead class="table-dark">
                         <tr>
                             <th rowspan="4" align="middle" style="width: 5%">Kode</th>
