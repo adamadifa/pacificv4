66a6bd5d99c2e3870528a209db7afc1ce32f5272
adam adifam <adamabdi.al.a@gmail.com>
2025-02-06 16:10:31 +0700

    update

--- app/Http/Controllers/PencairanprogramController.php
+++ app/Http/Controllers/PencairanprogramController.php
@@ -353,9 +353,9 @@ class PencairanprogramController extends Controller
             'marketing_penjualan.jenis_transaksi',
             'marketing_penjualan.kode_pelanggan',
             'nama_pelanggan',
-            DB::raw('floor(jumlah/isi_pcs_dus) as jml_dus'),
-            DB::raw('(SELECT diskon FROM produk_diskon WHERE floor(marketing_penjualan_detail.jumlah/produk.isi_pcs_dus) BETWEEN produk_diskon.min_qty AND produk_diskon.max_qty AND kode_kategori_diskon="' . $kategori_diskon . '") as diskon'),
-            DB::raw('floor(jumlah/isi_pcs_dus) * (SELECT diskon FROM produk_diskon WHERE floor(marketing_penjualan_detail.jumlah/produk.isi_pcs_dus) BETWEEN produk_diskon.min_qty AND produk_diskon.max_qty AND kode_kategori_diskon="' . $kategori_diskon . '") as diskon_reguler'),
+            DB::raw('SUM(floor(jumlah/isi_pcs_dus)) as jml_dus'),
+            DB::raw('(SELECT diskon FROM produk_diskon WHERE SUM(floor(marketing_penjualan_detail.jumlah/produk.isi_pcs_dus)) BETWEEN produk_diskon.min_qty AND produk_diskon.max_qty AND kode_kategori_diskon="' . $kategori_diskon . '") as diskon'),
+            DB::raw('SUM(floor(jumlah/isi_pcs_dus)) * (SELECT diskon FROM produk_diskon WHERE floor(marketing_penjualan_detail.jumlah/produk.isi_pcs_dus) BETWEEN produk_diskon.min_qty AND produk_diskon.max_qty AND kode_kategori_diskon="' . $kategori_diskon . '") as diskon_reguler'),
 
         )
             ->join('produk_harga', 'marketing_penjualan_detail.kode_harga', '=', 'produk_harga.kode_harga')
@@ -370,6 +370,14 @@ class PencairanprogramController extends Controller
             ->where('status_batal', 0)
             ->whereIn('produk_harga.kode_produk', $produk)
             ->orderBy('nama_pelanggan')
+            ->groupBy(
+                'marketing_penjualan.no_faktur',
+                'marketing_penjualan.tanggal',
+                'marketing_penjualan.tanggal_pelunasan',
+                'marketing_penjualan.jenis_transaksi',
+                'marketing_penjualan.kode_pelanggan',
+                'nama_pelanggan'
+            )
             ->get();
 
         return view('worksheetom.pencairanprogram.detailfaktur', compact('detailpenjualan'));
