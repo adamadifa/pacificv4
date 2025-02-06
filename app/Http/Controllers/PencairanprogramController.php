52381465ba6d18ca92b3548d9a8704cf63869a03
adam adifam <adamabdi.al.a@gmail.com>
2025-02-06 16:27:07 +0700

    update

--- app/Http/Controllers/PencairanprogramController.php
+++ app/Http/Controllers/PencairanprogramController.php
@@ -159,7 +159,7 @@ class PencairanprogramController extends Controller
         $detailpenjualan = Detailpenjualan::select(
             'marketing_penjualan.kode_pelanggan',
             'nama_pelanggan',
-            DB::raw('floor(jumlah/isi_pcs_dus) as jml_dus'),
+            DB::raw('SUM(floor(jumlah/isi_pcs_dus)) as jml_dus'),
             DB::raw('(SELECT diskon FROM produk_diskon WHERE floor(marketing_penjualan_detail.jumlah/produk.isi_pcs_dus) BETWEEN produk_diskon.min_qty AND produk_diskon.max_qty AND kode_kategori_diskon="' . $kategori_diskon . '") as diskon'),
             DB::raw('floor(jumlah/isi_pcs_dus) * (SELECT diskon FROM produk_diskon WHERE floor(marketing_penjualan_detail.jumlah/produk.isi_pcs_dus) BETWEEN produk_diskon.min_qty AND produk_diskon.max_qty AND kode_kategori_diskon="' . $kategori_diskon . '") as diskon_reguler'),
 
