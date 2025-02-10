697eb0f8227e9cd87b98fca0627338d92d7890c0
Adam Adifa <adamabdi.al.a@gmail.com>
2025-02-10 14:19:27 +0700

    update

--- app/Http/Controllers/OmancabangController.php
+++ app/Http/Controllers/OmancabangController.php
@@ -38,7 +38,7 @@ class OmancabangController extends Controller
 
         if (!$user->hasRole($roles_show_cabang)) {
             if ($user->hasRole('rsm')) {
-                $query->where('cabang.kode_regional', auth()->user()->kode_regional);
+                $query->where('cabang.kode_regionald', auth()->user()->kode_regional);
             } else {
                 $query->where('marketing_oman_cabang.kode_cabang', auth()->user()->kode_cabang);
             }
