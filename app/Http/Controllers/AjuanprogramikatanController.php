9ae04d3cd89934d3a75995883ec44e483b078d41
adam adifam <adamabdi.al.a@gmail.com>
2025-02-12 10:25:49 +0700

    update

--- app/Http/Controllers/AjuanprogramikatanController.php
+++ app/Http/Controllers/AjuanprogramikatanController.php
@@ -602,6 +602,11 @@ class AjuanprogramikatanController extends Controller
         $tahun = date('Y', strtotime($tanggal_ajuan));
         $tahunlalu = $tahun - 1;
         $produk = json_decode($programikatan->produk, true) ?? [];
+        $dari = $tahunlalu . "-" . date('m-d', strtotime($programikatan->periode_dari));
+        $sampai = $tahunlalu . "-" . date('m-d', strtotime($programikatan->periode_sampai));
+
+
+
         $detailpenjualan = Detailpenjualan::join('marketing_penjualan', 'marketing_penjualan_detail.no_faktur', '=', 'marketing_penjualan.no_faktur')
             ->join('produk_harga', 'marketing_penjualan_detail.kode_harga', '=', 'produk_harga.kode_harga')
             ->join('produk', 'produk_harga.kode_produk', '=', 'produk.kode_produk')
@@ -610,8 +615,9 @@ class AjuanprogramikatanController extends Controller
             ->whereIn('produk_harga.kode_produk', $produk)
             // ->where('marketing_penjualan.kode_pelanggan', $kode_pelanggan)
             // ->whereBetween('marketing_penjualan.tanggal', [$dari_lasttigabulan, $sampai_lastbulan])
+            ->whereBetween('marketing_penjualan.tanggal', [$dari, $sampai])
             ->whereIn('marketing_penjualan.kode_pelanggan', $list_pelanggan)
-            ->whereRaw('YEAR(marketing_penjualan.tanggal)="' . $tahunlalu . '"')
+            // ->whereRaw('YEAR(marketing_penjualan.tanggal)="' . $tahunlalu . '"')
             // ->where('salesman.kode_cabang', $programikatan->kode_cabang)
             ->where('status_promosi', 0)
             ->where('status_batal', 0)
@@ -622,6 +628,7 @@ class AjuanprogramikatanController extends Controller
             )
             ->groupBy('marketing_penjualan.kode_pelanggan', 'nama_pelanggan');
         $data['programikatan'] = $programikatan;
+
 
 
 
