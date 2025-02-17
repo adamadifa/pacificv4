cb1bd66b10abb26dc54ab8dd733833d52943c453
Adam Adifa <adamabdi.al.a@gmail.com>
2025-02-17 10:22:54 +0700

    update

--- resources/views/worksheetom/ajuanprogramkumulatif/cetakkesepakatan.blade.php
+++ resources/views/worksheetom/ajuanprogramkumulatif/cetakkesepakatan.blade.php
@@ -71,7 +71,7 @@
             </table>
 
             <h3 style="text-align: center">
-                SURAT KESEPAKATAN PROGRAM DISCOUNT KUMULATIF
+                SURAT PEMBERITAHUAN PROGRAM DISCOUNT KUMULATIF
                 <br>
                 PROGRAM {{ $kesepakatan->nama_program }}
             </h3>
@@ -81,19 +81,15 @@
                 <tr>
                     <td style="width: 40%">Nama Lengkap</td>
                     <td style="width: 1%">:</td>
-                    <td style="border-bottom: 1px solid black"></td>
+                    <td style="border-bottom: 1px solid black">{{ $kesepakatan->nama_salesman }}</td>
                 </tr>
 
                 <tr>
                     <td style="width: 40%">Alamat Lengkap Tempat Tinggal</td>
                     <td style="width: 1%">:</td>
-                    <td style="border-bottom: 1px solid black"></td>
+                    <td style="border-bottom: 1px solid black">{{ $kesepakatan->alamat_cabang }}</td>
                 </tr>
-                <tr>
-                    <td style="width: 40%"></td>
-                    <td style="width: 1%">:</td>
-                    <td style="border-bottom: 1px solid black"></td>
-                </tr>
+
                 <tr>
                     <td style="width: 40%">NIK KTP</td>
                     <td style="width: 1%">:</td>
