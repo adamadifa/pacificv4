4fdfafde6b02e768474f1c945a1109e893e4167f
Adam Adifa <adamabdi.al.a@gmail.com>
2025-02-22 08:00:44 +0700

    update

--- database/migrations/2025_02_22_050940_create_tickets_table.php
+++ database/migrations/2025_02_22_050940_create_tickets_table.php
@@ -21,6 +21,7 @@ return new class extends Migration
             $table->bigInteger('id_user');
             $table->date('tanggal_selesai')->nullable();
             $table->bigInteger('id_admin')->nullable();
+            $table->timestamps();
         });
     }
 
