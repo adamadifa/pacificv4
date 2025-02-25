44d1160197e6a78300421a44fad3c3f4ab8ef019
Adam Adifa <adamabdi.al.a@gmail.com>
2025-02-25 14:08:15 +0700

    update

--- resources/views/worksheetom/monitoringprogram/index.blade.php
+++ resources/views/worksheetom/monitoringprogram/index.blade.php
@@ -52,7 +52,8 @@
                                         </div>
                                     </div>
                                 </div>
-                                <x-input-with-icon icon="ti ti-user" label="Nama Pelanggan" name="nama_pelanggan" />
+                                <x-input-with-icon icon="ti ti-user" label="Nama Pelanggan" name="nama_pelanggan"
+                                    value="{{ Request('nama_pelanggan') }}" />
                                 <div class="row">
                                     <div class="col-lg-12 col-md-12 col-sm-12">
                                         <div class="form-group mb-3">
