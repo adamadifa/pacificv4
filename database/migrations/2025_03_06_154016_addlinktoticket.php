d686267977592286e357182dc9908831ab2aa4cb
adam adifam <adamabdi.al.a@gmail.com>
2025-03-06 15:47:57 +0700

    update

--- database/migrations/2025_03_06_154016_addlinktoticket.php
+++ database/migrations/2025_03_06_154016_addlinktoticket.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::table('ticket', function (Blueprint $table) {
+        Schema::table('tickets', function (Blueprint $table) {
             $table->text('link')->nullable();
         });
     }
@@ -21,7 +21,7 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::table('ticket', function (Blueprint $table) {
+        Schema::table('tickets', function (Blueprint $table) {
             $table->dropColumn('link');
         });
     }
