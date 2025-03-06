c5d4e0ef5d06139e77720f3764d011a1d0892e56
Adam Adifa <adamabdi.al.a@gmail.com>
2025-03-06 09:43:51 +0700

    update

--- resources/views/marketing/penjualan/create.blade.php
+++ resources/views/marketing/penjualan/create.blade.php
@@ -1268,7 +1268,8 @@
         function hitungdiskonSwan() {
             let totalQuantity = calculateTotalQuantityByCategory('D001');
             let diskon = calculateDiscount(totalQuantity, 'D001');
-            // let diskonbp500 = hitungdiskonProductBP500();
+            let diskonbp500 = calculateTotalQuantityByProduct('BP500');
+            console.log(diskonbp500);
             // let totaldiskon = parseInt(diskon) + parseInt(diskonbp500);
             $("#potongan_swan").val(convertToRupiah(diskon));
             return diskon;
