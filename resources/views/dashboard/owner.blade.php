a992fd79d5da9a2a78d497d170d367495e63e32b
Adam Adifa <adamabdi.al.a@gmail.com>
2025-04-21 13:27:55 +0700

    update

--- resources/views/dashboard/owner.blade.php
+++ resources/views/dashboard/owner.blade.php
@@ -81,7 +81,7 @@
                                         <tbody>
                                             @foreach ($bank as $d)
                                                 <tr>
-                                                    <th>{{ $d->nama_bank_alias || $d->nama_bank }} {{ $d->no_rekening }}
+                                                    <th>{{ $d->nama_bank_alias }} {{ $d->no_rekening }}
                                                     </th>
                                                     <td class="text-end">{{ formatRupiah($d->saldo) }}</td>
                                                 </tr>
