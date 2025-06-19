691b91d210b52ff81223ec5bb7090109fc968003
Adam Adifa <adamabdi.al.a@gmail.com>
2025-06-19 10:04:20 +0700

    update

--- resources/views/hrd/laporan/gaji_cetak.blade.php
+++ resources/views/hrd/laporan/gaji_cetak.blade.php
@@ -546,7 +546,7 @@
                                             $total_jam = !empty($cekdirumahkan)
                                                 ? $total_jam_jadwal / 2
                                                 : $total_jam_jadwal;
-                                            $potongan_jam_sakit = !empty($cekdirumahkan) ? $total_jam : 0;
+                                            $potongan_jam_sakit =  0;
                                             $keterangan = 'SID';
                                             if (!empty($cekdirumahkan)) {
                                                 $potongan_jam_dirumahkan = $total_jam_jadwal == 7 ? 1.75 : 1.25;
--- resources/views/hrd/laporan/presensi_cetak.blade.php
+++ resources/views/hrd/laporan/presensi_cetak.blade.php
@@ -446,7 +446,7 @@
                                                         ? $total_jam_jadwal / 2
                                                         : $total_jam_jadwal;
                                                 }
-                                                $potongan_jam_sakit = !empty($cekdirumahkan) ? $total_jam : 0;
+                                                $potongan_jam_sakit = 0;
 
                                                 if (!empty($cekdirumahkan)) {
                                                     $potongan_jam_dirumahkan = $total_jam_jadwal == 7 ? 1.75 : 1.25;
