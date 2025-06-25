1a63e1cacaa8e0155fb8d547b85ebbe126e0f02e
Adam Adifa <adamabdi.al.a@gmail.com>
2025-06-25 11:34:55 +0700

    update

--- app/Models/Izincuti.php
+++ app/Models/Izincuti.php
@@ -53,49 +53,115 @@ class Izincuti extends Model
         $query->join('hrd_departemen', 'hrd_izincuti.kode_dept', '=', 'hrd_departemen.kode_dept');
         $query->join('cabang', 'hrd_izincuti.kode_cabang', '=', 'cabang.kode_cabang');
         $query->join('hrd_jeniscuti', 'hrd_izincuti.kode_cuti', '=', 'hrd_jeniscuti.kode_cuti');
-        
+
         //dd(!in_array($role, $role_access_full));
         //Jika Admin Presensi
         //dd($cekPending);
         if (!empty($kode_izin_cuti)) {
             $query->where('hrd_izincuti.kode_izin_cuti', $kode_izin_cuti);
-        }
-        if (!$cekPending) {
-            if (!in_array($role, $role_access_full)) {
-                if ($user->can('izinabsen.create')) {
-                    if ($user->kode_cabang == 'PST') {
-                        $query->where('hrd_izincuti.kode_dept', $user->kode_dept);
+        } else {
+            if (!$cekPending) {
+                if (!in_array($role, $role_access_full)) {
+                    if ($user->can('izinabsen.create')) {
+                        if ($user->kode_cabang == 'PST') {
+                            $query->where('hrd_izincuti.kode_dept', $user->kode_dept);
+                        } else {
+                            $query->where('hrd_izincuti.kode_cabang', auth()->user()->kode_cabang);
+                        }
                     } else {
-                        $query->where('hrd_izincuti.kode_cabang', auth()->user()->kode_cabang);
-                    }
-                } else {
 
-                    if (!empty($request)) {
-                        if (!empty($request->dari) && !empty($request->sampai)) {
-                            $query->whereBetween('hrd_izincuti.tanggal', [$request->dari, $request->sampai]);
-                        }
-                        if (!empty($request->kode_cabang)) {
-                            $query->where('hrd_izincuti.kode_cabang', $request->kode_cabang);
-                        }
-                        if (!empty($request->kode_dept)) {
-                            $query->where('hrd_izincuti.kode_dept', $request->kode_dept);
-                        }
-                        if (!empty($request->nama_karyawan)) {
-                            $query->where('hrd_karyawan.nama_karyawan', 'like', '%' . $request->nama_karyawan . '%');
-                        }
-                        if (!empty($request->status)) {
+                        if (!empty($request)) {
+                            if (!empty($request->dari) && !empty($request->sampai)) {
+                                $query->whereBetween('hrd_izincuti.tanggal', [$request->dari, $request->sampai]);
+                            }
+                            if (!empty($request->kode_cabang)) {
+                                $query->where('hrd_izincuti.kode_cabang', $request->kode_cabang);
+                            }
+                            if (!empty($request->kode_dept)) {
+                                $query->where('hrd_izincuti.kode_dept', $request->kode_dept);
+                            }
+                            if (!empty($request->nama_karyawan)) {
+                                $query->where('hrd_karyawan.nama_karyawan', 'like', '%' . $request->nama_karyawan . '%');
+                            }
                             if (!empty($request->status)) {
-                                if ($request->status == 'pending') {
-                                    $query->where('hrd_izincuti.status', '0');
-                                } else if ($request->status == 'disetujui') {
-                                    $query->where('hrd_izincuti.status', '1');
+                                if (!empty($request->status)) {
+                                    if ($request->status == 'pending') {
+                                        $query->where('hrd_izincuti.status', '0');
+                                    } else if ($request->status == 'disetujui') {
+                                        $query->where('hrd_izincuti.status', '1');
+                                    }
                                 }
                             }
                         }
+
+                        $query->whereIn('hrd_izincuti.kode_dept', $dept_access);
+                        if ($jabatan_filter && $jabatan_access != null) {
+                            $query->whereIn('hrd_izincuti.kode_jabatan', $jabatan_access);
+                        }
+                        if ($cabang_access == 1) {
+                            $query->where('hrd_izincuti.kode_cabang', auth()->user()->kode_cabang);
+                        } else if ($cabang_access == 2) {
+                            $query->where('cabang.kode_regional', auth()->user()->kode_regional);
+                        }
+
+
+
+                        $query->orWhereIn('hrd_izincuti.kode_dept', $dept_access_2);
+                        if ($jabatan_filter && $jabatan_access_2 != null) {
+                            $query->whereIn('hrd_izincuti.kode_jabatan', $jabatan_access_2);
+                        }
+                        if ($cabang_access == 1) {
+                            $query->where('hrd_izincuti.kode_cabang', auth()->user()->kode_cabang);
+                        } else if ($cabang_access == 2) {
+                            $query->where('cabang.kode_regional', auth()->user()->kode_regional);
+                        }
+                    }
+                }
+
+                if ($role == 'direktur') {
+                    $query->where('hrd_izincuti.forward_to_direktur', '1');
+                }
+
+
+                if (!empty($request)) {
+                    if (!empty($request->dari) && !empty($request->sampai)) {
+                        $query->whereBetween('hrd_izincuti.tanggal', [$request->dari, $request->sampai]);
+                    }
+                    if (!empty($request->kode_cabang)) {
+                        $query->where('hrd_izincuti.kode_cabang', $request->kode_cabang);
+                    }
+                    if (!empty($request->kode_dept)) {
+                        $query->where('hrd_izincuti.kode_dept', $request->kode_dept);
+                    }
+                    if (!empty($request->nama_karyawan)) {
+                        $query->where('hrd_karyawan.nama_karyawan', 'like', '%' . $request->nama_karyawan . '%');
                     }
 
+                    if ($role == 'direktur') {
+                        if (!empty($request->status)) {
+                            if ($request->status == 'pending') {
+                                $query->where('hrd_izincuti.forward_to_direktur', '1');
+                                $query->where('hrd_izincuti.direktur', '0');
+                            } else if ($request->status == 'disetujui') {
+                                $query->where('hrd_izincuti.forward_to_direktur', '1');
+                                $query->where('hrd_izincuti.direktur', '1');
+                            }
+                        }
+                    } else {
+                        if (!empty($request->status)) {
+                            if ($request->status == 'pending') {
+                                $query->where('hrd_izincuti.status', '0');
+                            } else if ($request->status == 'disetujui') {
+                                $query->where('hrd_izincuti.status', '1');
+                            }
+                        }
+                    }
+                }
+            } else {
+                if (!in_array($role, $level_hrd) && $role !== 'direktur') {
+                    $query->where('hrd_izincuti.head', '0');
                     $query->whereIn('hrd_izincuti.kode_dept', $dept_access);
-                    if ($jabatan_filter && $jabatan_access != null) {
+                    if ($jabatan_access != null) {
                         $query->whereIn('hrd_izincuti.kode_jabatan', $jabatan_access);
                     }
                     if ($cabang_access == 1) {
@@ -103,11 +169,9 @@ class Izincuti extends Model
                     } else if ($cabang_access == 2) {
                         $query->where('cabang.kode_regional', auth()->user()->kode_regional);
                     }
-
-
-
-                    $query->orWhereIn('hrd_izincuti.kode_dept', $dept_access_2);
-                    if ($jabatan_filter && $jabatan_access_2 != null) {
+                    $query->orWhere('hrd_izincuti.head', '0');
+                    $query->whereIn('hrd_izincuti.kode_dept', $dept_access_2);
+                    if ($jabatan_access_2 != null) {
                         $query->whereIn('hrd_izincuti.kode_jabatan', $jabatan_access_2);
                     }
                     if ($cabang_access == 1) {
@@ -116,82 +180,17 @@ class Izincuti extends Model
                         $query->where('cabang.kode_regional', auth()->user()->kode_regional);
                     }
                 }
-            }
-
-            if ($role == 'direktur') {
-                $query->where('hrd_izincuti.forward_to_direktur', '1');
-            }
 
 
-            if (!empty($request)) {
-                if (!empty($request->dari) && !empty($request->sampai)) {
-                    $query->whereBetween('hrd_izincuti.tanggal', [$request->dari, $request->sampai]);
-                }
-                if (!empty($request->kode_cabang)) {
-                    $query->where('hrd_izincuti.kode_cabang', $request->kode_cabang);
-                }
-                if (!empty($request->kode_dept)) {
-                    $query->where('hrd_izincuti.kode_dept', $request->kode_dept);
-                }
-                if (!empty($request->nama_karyawan)) {
-                    $query->where('hrd_karyawan.nama_karyawan', 'like', '%' . $request->nama_karyawan . '%');
+                if (in_array($role, $level_hrd)) {
+                    $query->where('hrd_izincuti.head', '1');
+                    $query->where('hrd_izincuti.hrd', 0);
                 }
 
                 if ($role == 'direktur') {
-                    if (!empty($request->status)) {
-                        if ($request->status == 'pending') {
-                            $query->where('hrd_izincuti.forward_to_direktur', '1');
-                            $query->where('hrd_izincuti.direktur', '0');
-                        } else if ($request->status == 'disetujui') {
-                            $query->where('hrd_izincuti.forward_to_direktur', '1');
-                            $query->where('hrd_izincuti.direktur', '1');
-                        }
-                    }
-                } else {
-                    if (!empty($request->status)) {
-                        if ($request->status == 'pending') {
-                            $query->where('hrd_izincuti.status', '0');
-                        } else if ($request->status == 'disetujui') {
-                            $query->where('hrd_izincuti.status', '1');
-                        }
-                    }
+                    $query->where('forward_to_direktur', '1');
+                    $query->where('direktur', 0);
                 }
-            }
-
-           
-        } else {
-            if (!in_array($role, $level_hrd) && $role !== 'direktur') {
-                $query->where('hrd_izincuti.head', '0');
-                $query->whereIn('hrd_izincuti.kode_dept', $dept_access);
-                if ($jabatan_access != null) {
-                    $query->whereIn('hrd_izincuti.kode_jabatan', $jabatan_access);
-                }
-                if ($cabang_access == 1) {
-                    $query->where('hrd_izincuti.kode_cabang', auth()->user()->kode_cabang);
-                } else if ($cabang_access == 2) {
-                    $query->where('cabang.kode_regional', auth()->user()->kode_regional);
-                }
-                $query->orWhere('hrd_izincuti.head', '0');
-                $query->whereIn('hrd_izincuti.kode_dept', $dept_access_2);
-                if ($jabatan_access_2 != null) {
-                    $query->whereIn('hrd_izincuti.kode_jabatan', $jabatan_access_2);
-                }
-                if ($cabang_access == 1) {
-                    $query->where('hrd_izincuti.kode_cabang', auth()->user()->kode_cabang);
-                } else if ($cabang_access == 2) {
-                    $query->where('cabang.kode_regional', auth()->user()->kode_regional);
-                }
-            }
-
-
-            if (in_array($role, $level_hrd)) {
-                $query->where('hrd_izincuti.head', '1');
-                $query->where('hrd_izincuti.hrd', 0);
-            }
-
-            if ($role == 'direktur') {
-                $query->where('forward_to_direktur', '1');
-                $query->where('direktur', 0);
             }
         }
 
