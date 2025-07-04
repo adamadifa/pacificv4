620b3236ea6d7064a18f2376a46dba062d7ac3e7
Adam Adifa <adamabdi.al.a@gmail.com>
2025-07-04 09:44:10 +0700

    update

--- app/Http/Controllers/SaldokasbesarkeuanganController.php
+++ app/Http/Controllers/SaldokasbesarkeuanganController.php
@@ -71,8 +71,8 @@ class SaldokasbesarkeuanganController extends Controller
         $user = User::findorfail(auth()->user()->id);
         $data['user'] = $user;
         
-      
-        if(request()->is('/sakasbesarkeuangan/createpusat')){
+        
+        if(request()->is('sakasbesarkeuangan/createpusat')){
             return view('keuangan.mutasikeuangan.sakasbesarkeuangan.createpusat', $data);
         }else{
 
