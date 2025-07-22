9579f11de5d934b060bd603830fc8aeaad2432f5
Adam Adifa <adamabdi.al.a@gmail.com>
2025-07-22 13:58:09 +0700

    update

--- resources/views/worksheetom/pencairanprogramenambulan/cetak.blade.php
+++ resources/views/worksheetom/pencairanprogramenambulan/cetak.blade.php
@@ -83,165 +83,51 @@
         <!-- "padding-**mm" is optional: you can set 10, 15, 20 or 25 -->
         <section class="sheet padding-10mm">
 
-            <table class="datatable3">
+            <table class="table">
                 <tr>
-                    <td>Kode Pencairan</td>
-                    <td class="right">{{ $pencairanprogram->kode_pencairan }}</td>
+                    <th>Kode Pencairan</th>
+                    <td class="text-end">{{ $pencairanprogram->kode_pencairan }}</td>
                 </tr>
                 <tr>
-                    <td>Tanggal</td>
-                    <td class="right">{{ DateToIndo($pencairanprogram->tanggal) }}</td>
+                    <th>Tanggal</th>
+                    <td class="text-end">{{ DateToIndo($pencairanprogram->tanggal) }}</td>
                 </tr>
                 <tr>
-                    <td>Periode Penjualan</td>
-                    <td class="right">{{ $namabulan[$pencairanprogram->bulan] }} {{ $pencairanprogram->tahun }}</td>
-                </tr>
-
-                <tr>
-                    <td>Program</td>
-                    <td class="right">{{ $pencairanprogram->nama_program }}</td>
-                </tr>
-                <tr>
-                    <td>Cabang</td>
-                    <td class="right">{{ strtoupper($pencairanprogram->nama_cabang) }}</td>
-                </tr>
-            </table>
-            <br>
-            <br>
-
-            <table class="datatable3" style="width: 100%">
-                <thead style="background-color: #055b90; color:white">
-                    <tr>
-                        <th rowspan="2">No.</th>
-                        <th rowspan="2">Kode</th>
-                        <th rowspan="2">Nama Pelanggan</th>
-                        <th colspan="3" class="text-center">Budget</th>
-                        <th rowspan="2" class="text-center">Target</th>
-                        <th class="text-center" colspan="3">Realisasi</th>
-                        <th colspan="3" class="text-center">Reward</th>
-
-                        <th rowspan="2">Pembayaran</th>
-                        <th rowspan="2">No. Rekening</th>
-                        <th rowspan="2">Pemilik</th>
-                        <th rowspan="2">Bank</th>
-                    </tr>
-                    <tr>
-                        <th>SMM</th>
-                        <th>RSM</th>
-                        <th>GM</th>
-                        <th>Tunai</th>
-                        <th>Kredit</th>
-                        <th>Total</th>
-                        <th>Tunai</th>
-                        <th>Kredit</th>
-                        <th>Total</th>
-                    </tr>
-
-                </thead>
-                <tbody id="loaddetailpencairan">
-                    @php
-                        $metode_pembayaran = [
-                            'TN' => 'Tunai',
-                            'TF' => 'Transfer',
-                            'VC' => 'Voucher',
-                        ];
-                        $subtotal_reward = 0;
-                        $grandtotal_reward = 0;
-                        $grandtotal_reward_tunai = 0;
-                        $grandtotal_reward_kredit = 0;
-                        $subtotal_reward_tunai = 0;
-                        $subtotal_reward_kredit = 0;
-                        $grandtotal_transfer = 0;
-                        $grandtotal_tunai = 0;
-
-                        $bb_dep = ['PRIK004', 'PRIK001'];
-                    @endphp
-                    @foreach ($detail as $key => $d)
-                        @php
-                            $next_metode_pembayaran = @$detail[$key + 1]->metode_pembayaran;
-                            $total_reward =
-                                $d->total_reward > 1000000 && !in_array($d->kode_program, $bb_dep)
-                                    ? 1000000
-                                    : $d->total_reward;
-                            $subtotal_reward_tunai += $d->reward_tunai;
-                            $subtotal_reward_kredit += $d->reward_kredit;
-                            $subtotal_reward += $total_reward;
-                            $grandtotal_reward += $total_reward;
-                            $grandtotal_reward_tunai += $d->reward_tunai;
-                            $grandtotal_reward_kredit += $d->reward_kredit;
-                            $bgcolor = $d->status_pencairan == 0 ? 'yellow' : '';
-                            $grandtotal_transfer += $d->metode_pembayaran == 'TF' ? $total_reward : 0;
-                            $grandtotal_tunai += $d->metode_pembayaran == 'TN' ? $total_reward : 0;
-                        @endphp
-                        <tr style="background-color: {{ $bgcolor }}">
-                            <td>{{ $loop->iteration }}</td>
-                            <td>{{ $d->kode_pelanggan }}</td>
-                            <td>{{ $d->nama_pelanggan }}</td>
-                            <td class="right">{{ formatAngka($d->budget_smm) }}</td>
-                            <td class="right">{{ formatAngka($d->budget_rsm) }}</td>
-                            <td class="right">{{ formatAngka($d->budget_gm) }}</td>
-                            <td class="text-center">{{ formatAngka($d->qty_target) }}</td>
-                            <td class="text-center">{{ formatAngka($d->qty_tunai) }}</td>
-                            <td class="text-center">{{ formatAngka($d->qty_kredit) }}</td>
-                            <td class="text-center">{{ formatAngka($d->jumlah) }}</td>
-                            <td class="right">{{ formatAngka($d->reward_tunai) }}</td>
-                            <td class="right">{{ formatAngka($d->reward_kredit) }}</td>
-                            <td class="right">{{ formatAngka($total_reward) }}</td>
-                            <td>{{ $metode_pembayaran[$d->metode_pembayaran] }}</td>
-
-                            <td>'{{ $d->no_rekening }}</td>
-                            <td>{{ $d->pemilik_rekening }}</td>
-                            <td>{{ $d->bank }}</td>
-                        </tr>
-                        @if ($d->metode_pembayaran != $next_metode_pembayaran)
-                            <tr class="table-dark" style="background-color: #055b90; color:white">
-                                <th colspan="10">TOTAL REWARD </th>
-                                <th class="right">{{ formatAngka($subtotal_reward_tunai) }}</th>
-                                <th class="right">{{ formatAngka($subtotal_reward_kredit) }}</th>
-                                <th class="right">{{ formatAngka($subtotal_reward) }}</th>
-                                <th colspan="4"></th>
-                            </tr>
+                    <th>Periode Penjualan</th>
+                    <td class="text-end">
+                        @if ($pencairanprogram->semester == 1)
                             @php
-                                $subtotal_reward = 0;
-                                $subtotal_reward_tunai = 0;
-                                $subtotal_reward_kredit = 0;
+                                $periode_start = $pencairanprogram->tahun . '-01-01';
+                                $periode_end = date('Y-m-t', strtotime($pencairanprogram->tahun . '-06-01'));
                             @endphp
                         @endif
-                    @endforeach
-                </tbody>
-                <tfoot class="table-dark" style="background-color: #055b90; color:white">
-                    <tr>
-                        <th colspan="10">GRAND TOTAL REWARD </th>
-                        <th class="right">{{ formatAngka($grandtotal_reward_tunai) }}</th>
-                        <th class="right">{{ formatAngka($grandtotal_reward_kredit) }}</th>
-                        <th class="right">{{ formatAngka($grandtotal_reward) }}</th>
-                        <th colspan="4"></th>
-                    </tr>
-                </tfoot>
+                        @if ($pencairanprogram->semester == 2)
+                            @php
+                                $periode_start = $pencairanprogram->tahun . '-07-01';
+                                $periode_end = date('Y-m-t', strtotime($pencairanprogram->tahun . '-12-01'));
+                            @endphp
+                        @endif
+                        {{ DateToIndo($periode_start) }} s/d {{ DateToIndo($periode_end) }}
+                    </td>
+                </tr>
+                <tr>
+                    <th>No. Dokumen</th>
+                    <td class="text-end">{{ $pencairanprogram->nomor_dokumen }}</td>
+                </tr>
+                <tr>
+                    <th>Program</th>
+                    <td class="text-end">{{ $pencairanprogram->nama_program }}</td>
+                </tr>
+                <tr>
+                    <th>Cabang</th>
+                    <td class="text-end">{{ strtoupper($pencairanprogram->nama_cabang) }}</td>
+                </tr>
+
             </table>
             <br>
             <br>
-            <table class="datatable3">
-                <tr>
-                    <th colspan="2">REKAP PEMBAYARAN</th>
-                </tr>
-                <tr>
-                    <th>TRANSFER</th>
-                    <td class="right">{{ formatAngka($grandtotal_transfer) }}</td>
-                </tr>
-                <tr>
-                    <th>TUNAI</th>
-                    <td class="right">{{ formatAngka($grandtotal_tunai) }}</td>
-                </tr>
-            </table>
-            <br>
 
-            <br>
-            <br>
-
-        </section>
-        <section class="sheet padding-10mm">
-            <table class="datatable3" style="width:100%">
+            <table id="example" class="display nowrap table table-striped table-bordered" style="width:100%">
                 <thead class="table-dark">
                     <tr>
                         <th rowspan="2">No.</th>
@@ -302,8 +188,7 @@
                             <td class="text-center">{{ formatAngka($d->qty_tunai) }}</td>
                             <td class="text-center">{{ formatAngka($d->qty_kredit) }}</td>
                             <td class="text-center">
-                                <a href="#" class="btnDetailfaktur"
-                                    kode_pelanggan="{{ $d['kode_pelanggan'] }}">
+                                <a href="#" class="btnDetailfaktur" kode_pelanggan="{{ $d['kode_pelanggan'] }}">
                                     {{ formatAngka($d->jumlah) }}
                                 </a>
                             </td>
@@ -379,7 +264,28 @@
                                     </tr>
                                 </tfoot> --}}
             </table>
+            <br>
+            <br>
+            {{-- <table class="datatable3">
+                <tr>
+                    <th colspan="2">REKAP PEMBAYARAN</th>
+                </tr>
+                <tr>
+                    <th>TRANSFER</th>
+                    <td class="right">{{ formatAngka($grandtotal_transfer) }}</td>
+                </tr>
+                <tr>
+                    <th>TUNAI</th>
+                    <td class="right">{{ formatAngka($grandtotal_tunai) }}</td>
+                </tr>
+            </table> --}}
+            <br>
+
+            <br>
+            <br>
+
         </section>
+
     </body>
 
 </html>
