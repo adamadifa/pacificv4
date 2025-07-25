34bac0862166d4ec63ef3d8b30c3d560c0f344ee
Adam Adifa <adamabdi.al.a@gmail.com>
2025-07-25 09:02:12 +0700

    update

--- app/Http/Controllers/PencairanprogramenambulanController.php
+++ app/Http/Controllers/PencairanprogramenambulanController.php
@@ -240,7 +240,7 @@ class PencairanprogramenambulanController extends Controller
                 'pelanggan.nama_pelanggan',
                 'pelanggan.no_rekening',
                 'pelanggan.pemilik_rekening',
-                'pelanggan.bank',
+                'pelanggan.bankd',
                 'top',
                 'metode_pembayaran',
                 'qty_target',
