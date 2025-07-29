cb16e3934701d62660ba427c94fcc1c195ddb671
Adam Adifa <adamabdi.al.a@gmail.com>
2025-07-29 15:29:25 +0700

    update

--- resources/views/hrd/laporan/gaji_cetak.blade.php
+++ resources/views/hrd/laporan/gaji_cetak.blade.php
@@ -320,7 +320,6 @@
 
                                 if (!empty($cekliburnasional)) {
                                     if($d['kode_jabatan'] == 'J20'){
-                                        // $overtime_libur = $lembur_libur['overtime_libur'] * 2;
                                         $overtime_libur = $lembur_libur['overtime_libur'] * 2;
                                     }else{
                                         $overtime_libur = $lembur_libur['overtime_libur'];
