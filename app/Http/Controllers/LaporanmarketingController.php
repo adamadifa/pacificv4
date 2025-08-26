ba3e61961d4b06335ef387d661f99a196eb96241
Adam Adifa <adamabdi.al.a@gmail.com>
2025-08-27 04:24:00 +0700

    update

--- app/Http/Controllers/LaporanmarketingController.php
+++ app/Http/Controllers/LaporanmarketingController.php
@@ -4741,8 +4741,10 @@ class LaporanmarketingController extends Controller
             return view('marketing.laporan.komisi_salesman_cetak', $data);
         } else if ($request->bulan == 4 && $request->tahun == 2025) {
             return view('marketing.laporan.komisi_salesman_april_cetak', $data);
-        } else {
+        } else if ($request->bulan >= 5 && $request->bulan <= 8 && $request->tahun == 2025) {
             return view('marketing.laporan.komisi_salesman_mei_cetak', $data);
+        } else {
+            return view('marekting.laporan.komisi_salesman_agustus_cetak', $data);
         }
     }
 
