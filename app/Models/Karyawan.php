03c5459745b01e92db95f3fb1b8a31bcc6a2ac58
Adam Adifa <adamabdi.al.a@gmail.com>
2025-09-13 09:52:20 +0700

    update

--- app/Models/Karyawan.php
+++ app/Models/Karyawan.php
@@ -44,7 +44,7 @@ class Karyawan extends Model
                 $query->where('hrd_karyawan.kode_jabatan', 'J07');
                 $query->where('cabang.kode_regional', auth()->user()->kode_regional);
             } else if ($user->hasRole('regional operation manager')) { //REG. OPERATION MANAGER
-                $query->where('hrd_karyawan.kode_dept', 'AKT');
+                // $query->where('hrd_karyawan.kode_dept', 'AKT');
                 $query->where('hrd_karyawan.kode_jabatan', 'J08');
             } else if ($user->hasRole('manager keuangan')) { //MANAGER KEUANGAN
                 $query->whereIn('hrd_karyawan.kode_dept', ['AKT', 'KEU']);
