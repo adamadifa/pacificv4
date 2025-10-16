9519c411bf2fe37b3d2f797f909026624cc1d452
Adam Adifa <adamabdi.al.a@gmail.com>
2025-10-16 08:09:46 +0700

    update

--- app/Http/Controllers/LaporanaccountingController.php
+++ app/Http/Controllers/LaporanaccountingController.php
@@ -1666,15 +1666,15 @@ class LaporanaccountingController extends Controller
             $biaya_operasional = $subtotal_akun_biaya_adm + $subtotal_akun_biaya_penjualan;
             $operating_profit = $gross_profit - $biaya_operasional;
             $net_profit_loss = $operating_profit + $subtotal_akun_pendapatanlain - $subtotal_akun_biayalain;
-            echo "Pendapatan: " . $subtotal_akun_pendapatan . "<br>";
-            echo "Pokok Penjualan: " . $subtotal_akun_pokok_penjualan . "<br>";
-            echo "Gross Profit: " . $gross_profit . "<br>";
-            echo "Biaya Operasional: " . $biaya_operasional . "<br>";
-            echo "Operating Profit: " . $operating_profit . "<br>";
-            echo "Pendapatan Lain: " . $subtotal_akun_pendapatanlain . "<br>";
-            echo "Biaya Lain: " . $subtotal_akun_biayalain . "<br>";
-            echo "Net Profit Loss: " . $net_profit_loss . "<br>";
-            die;
+            // echo "Pendapatan: " . $subtotal_akun_pendapatan . "<br>";
+            // echo "Pokok Penjualan: " . $subtotal_akun_pokok_penjualan . "<br>";
+            // echo "Gross Profit: " . $gross_profit . "<br>";
+            // echo "Biaya Operasional: " . $biaya_operasional . "<br>";
+            // echo "Operating Profit: " . $operating_profit . "<br>";
+            // echo "Pendapatan Lain: " . $subtotal_akun_pendapatanlain . "<br>";
+            // echo "Biaya Lain: " . $subtotal_akun_biayalain . "<br>";
+            // echo "Net Profit Loss: " . $net_profit_loss . "<br>";
+            // die;
 
             $data['net_profit_loss'] = $net_profit_loss;
             //Neraca
