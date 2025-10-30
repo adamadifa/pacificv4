1c29cea348ac3dcc97127dbe482a1fb3fa74d2ae
Adam Adifa <adamabdi.al.a@gmail.com>
2025-10-30 14:08:01 +0700

    update

--- resources/views/maintenance/laporan/rekapbahanbakar_cetak.blade.php
+++ resources/views/maintenance/laporan/rekapbahanbakar_cetak.blade.php
@@ -111,7 +111,7 @@
                                 $total_harga_lainnya += $jumlah_lainnya;
                             @endphp
                             {{ formatAngkaDesimal($jumlah_lainnya) }}
-                            {{ $d['qty_lainnya'] }} * {{ $d['harga_lainnya'] }} = {{ $jumlah_lainnya }}
+                            {{-- {{ $d['qty_lainnya'] }} * {{ $d['harga_lainnya'] }} = {{ $jumlah_lainnya }} --}}
                         </td>
 
                         <td class="right">{{ formatAngkaDesimal($d['qty_keluar']) }}</td>
