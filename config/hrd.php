176b3fdc239876a4df83191d6c586cec14c35127
Adam Adifa <adamabdi.al.a@gmail.com>
2026-05-16 20:19:39 +0700

    update

--- config/hrd.php
+++ config/hrd.php
@@ -1,7 +1,7 @@
 <?php
 return [
     'pihak_satu' => [
-        'nama_pihak_satu' =>  'Dedy Julfikar',
+        'nama_pihak_satu' => 'Dedy Julfikar',
         'jabatan_pihak_satu' => 'Asst. HRD Manager',
     ],
 
@@ -11,7 +11,7 @@ return [
     'roles_approve_akt_cabang' => ['operation manager', 'manager keuangan', 'gm administrasi', 'direktur'],
     'roles_approve_mkt_cabang' => ['sales marketing manager', 'regional sales manager', 'gm marketing', 'direktur'],
 
-    'dept_lembur' =>  ['GDG', 'PRD', 'PDQ', 'GAF', 'MTC', 'HRD'],
+    'dept_lembur' => ['GDG', 'PRD', 'PDQ', 'GAF', 'MTC', 'HRD', 'PMB'],
     'roles_approve_presensi' => [
         'operation manager',
         'sales marketing manager',
