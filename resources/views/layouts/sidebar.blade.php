962fbf5eb0560e5a714ce3727696f083ea0bda89
Adam Adifa <adamabdi.al.a@gmail.com>
2026-09-15 15:43:16 +0700

    update

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -481,12 +481,12 @@
          </li>
          @endcan
 
-         <li class="menu-item {{ request()->is(['wa-komplain', 'wa-komplain/*']) ? 'active' : '' }}">
+         {{-- <li class="menu-item {{ request()->is(['wa-komplain', 'wa-komplain/*']) ? 'active' : '' }}">
              <a href="{{ route('wa-komplain.index') }}" class="menu-link">
                  <i class="menu-icon tf-icons ti ti-brand-whatsapp"></i>
                  <div>Komplain WA</div>
              </a>
-         </li>
+         </li> --}}
 
          @if (auth()->user()->hasRole(['super admin', 'gm administrasi']))
              <li class="menu-item {{ request()->is(['panduan', 'panduan/*']) ? 'active' : '' }}">
